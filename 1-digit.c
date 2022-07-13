5e9a60b62e9ccb36aa49e3ebe9b04d8ed7cfb29d
Joel Aisien <106709241+Joel1Aisien@users.noreply.github.com>
2022-07-13 09:00:59 +0100

    Update 1-digit.c

--- 1-digit.c
+++ 1-digit.c
@@ -17,4 +17,10 @@ void init_params(params_t *params, va_list ap)
 	params->zero_flag = 0;
 	params->minus_flag = 0;
 
-	
+	params->width = 0;
+	params->precision = UINT_MAX;
+
+	params->h_modifier = 0;
+	params->l_modifier = 0;
+	(void)ap;
+}
